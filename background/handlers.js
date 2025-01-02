375cd4a403dc335a7cdfcccb25260b645123d853
dev_tej <112886635+devTejaSrinivas@users.noreply.github.com>
2025-01-02 10:27:11 +0530

    Added basic set and get preference methods

---
+++ background/handlers.js
@@ -0,0 +1,16 @@
+// Get User Preference (for setting default) from UI Modules
+
+// Sets the user preferred method
+function saveUserPreference(method) {
+  chrome.storage.sync.set({ preferredMethod: method }, () => {
+    console.log("User preference saved");
+  });
+}
+
+// Gets the user preferred method
+function getUserPreference(callback) {
+  chrome.storage.sync.get("preferredMethod", (data) => {
+    const preferredMethod = data.preferredMethod || "abstractive";
+    console.log(`User preferred method is : ${preferredMethod}`);
+  });
+}
